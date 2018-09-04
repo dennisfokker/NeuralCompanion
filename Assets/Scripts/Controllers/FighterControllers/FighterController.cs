02a1db37feded52a846e0a7ac6c4d5e56bb156ab
TheDutcher <dennis.fokker@live.nl>
2018-09-04 15:21:30 +0200

    Fixed gitignore.

---
+++ Assets/Scripts/Controllers/FighterControllers/FighterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FighterController : MonoBehaviour
+{
+    public bool FlipRenderer = false;
+    public Color FighterColor = Color.white;
+
+    public GameManager GameManager { get; private set; }
+    public SpriteRenderer BackgroundRenderer { get; private set; }
+    public SpriteRenderer ActionRenderer { get; private set; }
+
+    public void Awake()
+    {
+        ActionRenderer = GetComponent<SpriteRenderer>();
+        GameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        if (FlipRenderer)
+            transform.localScale = Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
+        ActionRenderer.color = FighterColor;
+    }
+
+    public abstract IEnumerator PerformTurn(Action callback);
+}
---
+++ Assets/Scripts/Controllers/FighterControllers/PlayerFighterController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFighterController : FighterController
+{
+    public override IEnumerator PerformTurn(Action callback)
+    {
+        callback();
+        yield break;
+    }
+}
